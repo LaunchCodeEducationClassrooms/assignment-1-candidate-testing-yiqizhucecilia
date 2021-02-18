b565eab79a6ffd090811e87a81f91505f9431db8
Yiqi Zhu (Cecilia) <zhuyiqi28@gmail.com>
2021-02-18 21:56:18 +0000

    update

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,13 +2,13 @@ const input = require(`readline-sync`);
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName='';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer="hello";
 
-let questions=["1)Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers.","3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"]
+let questions=["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers.","(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"]
 let correctAnswers=["Sally Ride", "true", "40","Trajectory","3"]
 var candidateAnswers=new Array(5);
 
