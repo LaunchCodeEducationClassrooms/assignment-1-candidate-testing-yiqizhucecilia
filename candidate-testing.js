aa32adea050b475a31c39dcf0052a13353ffefac
Yiqi Zhu (Cecilia) <zhuyiqi28@gmail.com>
2021-02-18 21:50:16 +0000

    correct some mistakes

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,5 +1,4 @@
 const input = require(`readline-sync`);
-let count=0
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
@@ -7,9 +6,10 @@ let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
-let candidateAnswer;
-let questions=["Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers.","3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"]
-let correctAnswers=["Sally Ride", "True", "40","Trajectory","3"]
+let candidateAnswer="hello";
+
+let questions=["1)Who was the first American woman in space? ", "2) True or false: 5000 meters = 5 kilometers.","3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"]
+let correctAnswers=["Sally Ride", "true", "40","Trajectory","3"]
 var candidateAnswers=new Array(5);
 
 function askForName() {
@@ -29,15 +29,13 @@ for (i=0; i<5; i++){
 }
 
 function gradeQuiz(candidateAnswers) {
+  let count=0
   for (i=0; i<5; i++){
       if (candidateAnswers[i].toLowerCase()==correctAnswers[i].toLowerCase()){
-      count++}    
+      count=count+1}    
     }// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
   let grade=(count/correctAnswers.length)*100;
-  //return grade;
-  console.log(">>> Overall Grade:" +grade+ "% (" + count +" of " +correctAnswers.length + " responses correct) <<<" )
-
-  if (grade<80){console.log(">>> Status: FAILED <<<")}else(console.log(">>> Status: PASSED <<<"))
+  return grade;
 }
 
 function runProgram() {
@@ -47,6 +45,11 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
   
+  grade=gradeQuiz(this.candidateAnswers)
+  count=gradeQuiz(this.candidateAnswer).count
+  console.log(">>> Overall Grade:" +grade+ "% (" + count +" of " +correctAnswers.length + " responses correct) <<<" )
+
+  if (grade<80){console.log(">>> Status: FAILED <<<")}else(console.log(">>> Status: PASSED <<<"))
 }
 
 // Don't write any code below this line //
